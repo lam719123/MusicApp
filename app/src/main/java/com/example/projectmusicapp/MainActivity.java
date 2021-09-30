a0cac45e3fd0bcb21e4f408a2331419f91243c5b
TinkMerrrr <thaiminhtoexcellent@gmail.com>
2021-09-30 16:44:08 +0700

    Tap Song Dieu

--- app/src/main/java/com/example/projectmusicapp/MainActivity.java
+++ app/src/main/java/com/example/projectmusicapp/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.projectmusicapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
 
 import com.example.projectmusicapp.Adapter.MainViewPagerAdapter;
 import com.example.projectmusicapp.Fragment.DashboardFragment;
@@ -12,6 +17,10 @@ import com.example.projectmusicapp.Fragment.HomeFragment;
 import com.example.projectmusicapp.Fragment.LoadingDialog;
 import com.example.projectmusicapp.Fragment.PremiumFragment;
 import com.google.android.material.tabs.TabLayout;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
@@ -26,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
         loadingdialog.StartLoadingDialog();
         Handler handler = new Handler();
         handler.postDelayed(() -> loadingdialog.dismissDialog(), 3000);
+        printKeyHash(MainActivity.this);
 
     }
+
     private void init(){
         MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
         mainViewPagerAdapter.addFragment(new HomeFragment(),"");
@@ -43,5 +54,37 @@ public class MainActivity extends AppCompatActivity {
     private void Catch(){
         tabLayout = findViewById(R.id.myTabLayout);
         viewPager = findViewById(R.id.myViewPager);
+    }
+
+    public static String printKeyHash(Activity context) {
+        PackageInfo packageInfo;
+        String key = null;
+        try {
+            //getting application package name, as defined in manifest
+            String packageName = context.getApplicationContext().getPackageName();
+
+            //Retriving package info
+            packageInfo = context.getPackageManager().getPackageInfo(packageName,
+                    PackageManager.GET_SIGNATURES);
+
+            Log.e("Package Name=", context.getApplicationContext().getPackageName());
+
+            for (android.content.pm.Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                key = new String(Base64.encode(md.digest(), 0));
+
+                // String key = new String(Base64.encodeBytes(md.digest()));
+                Log.e("Key Hash=", key);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("Name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("No such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("Exception", e.toString());
+        }
+
+        return key;
     }
 }
---
+++ app/src/main/java/com/example/projectmusicapp/ShareFbLayoutActivity.java
@@ -0,0 +1,161 @@
+package com.example.projectmusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.share.model.ShareContent;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class ShareFbLayoutActivity extends AppCompatActivity {
+    private static ShareFbLayoutActivity shareFbLayoutActivity;
+    private CallbackManager callbackManager;
+    private FacebookCallback<LoginResult> loginResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_fb_layout);
+        printKeyHash(ShareFbLayoutActivity.this);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        shareFbLayoutActivity = this;
+        initFaceBook();
+        LoginManager.getInstance().registerCallback(callbackManager, loginResult);
+        Button loginButton = (Button) findViewById(R.id.btnLoginFb);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginFaceBook();
+            }
+        });
+        printKeyHash(this);
+    }
+    public void initFaceBook () {
+        loginResult = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //Login thành công xử lý tại đây
+                GraphRequest request = GraphRequest.newMeRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object,
+                                                    GraphResponse response) {
+                                // Application code
+                                String name = object.optString(getString(R.string.name));
+                                String id = object.optString(getString(R.string.id));
+                                String email = object.optString(getString(R.string.email));
+                                String link = object.optString(getString(R.string.link));
+                                URL imageURL = extractFacebookIcon(id);
+                                Log.d("name: ",name);
+                                Log.d("id: ",id);
+                                Log.d("email: ",email);
+                                Log.d("link: ",link);
+                                Log.d("imageURL: ",imageURL.toString());
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString(getString(R.string.fields), getString(R.string.fields_name));
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        };
+    }
+    public static String printKeyHash(Activity context) {
+        PackageInfo packageInfo;
+        String key = null;
+        try {
+            //getting application package name, as defined in manifest
+            String packageName = context.getApplicationContext().getPackageName();
+
+            //Retriving package info
+            packageInfo = context.getPackageManager().getPackageInfo(packageName,
+                    PackageManager.GET_SIGNATURES);
+
+            Log.e("Package Name=", context.getApplicationContext().getPackageName());
+
+            for (android.content.pm.Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                key = new String(Base64.encode(md.digest(), 0));
+
+                // String key = new String(Base64.encodeBytes(md.digest()));
+                Log.e("Key Hash=", key);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("Name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("No such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("Exception", e.toString());
+        }
+
+        return key;
+    }
+    public URL extractFacebookIcon(String id) {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            URL imageURL = new URL("http://graph.facebook.com/" + id
+                    + "/picture?type=large");
+            return imageURL;
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+    //Hàm check login facebook
+    public boolean isLoggedInFaceBook() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+    //Login facebook with permisstion
+    public void loginFaceBook() {
+        LoginManager.getInstance().logInWithReadPermissions(shareFbLayoutActivity, Arrays.asList("public_profile", "user_friends","email"));
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
